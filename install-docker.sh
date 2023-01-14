dcbfd16c86d67394c569147439256f3aa3793f07
Luigigimmi <gimmiluigi@gmail.com>
2023-01-14 21:33:27 +0000

    Run `apt-get update`

--- install-docker.sh
+++ install-docker.sh
@@ -6,7 +6,9 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# apt-cache madison docker-ce
+apt-get update
+
+apt-cache madison docker-ce
 
 apt-get install -y --allow-downgrades docker-ce=5:19.03.9~3-0~ubuntu-focal
 apt-get install -y --allow-downgrades docker-ce-cli=5:19.03.9~3-0~ubuntu-focal
